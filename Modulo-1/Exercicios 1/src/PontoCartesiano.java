86c39d4d627859dfd031d2eee136163f7ad6974a
kamyq <kamysbdt@gmail.com>
2024-03-29 17:35:57 -0300

    Separando o metodo main em uma nova classe

--- Modulo-1/Exercicios 1/src/PontoCartesiano.java
+++ Modulo-1/Exercicios 1/src/PontoCartesiano.java
@@ -1,4 +1,3 @@
-import java.util.Scanner;
 import java.lang.Math;
 
 public class PontoCartesiano {
@@ -31,25 +30,5 @@ public class PontoCartesiano {
         return distancia;
     }
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Digite as coordenadas do ponto 1 (x y):");
-        double x1 = scanner.nextDouble();
-        double y1 = scanner.nextDouble();
-        PontoCartesiano pontoUm = new PontoCartesiano(x1, y1);
-
-        System.out.println("Digite as coordenadas do ponto 2 (x y):");
-        double x2 = scanner.nextDouble();
-        double y2 = scanner.nextDouble();
-        PontoCartesiano pontoDois = new PontoCartesiano(x2, y2);
-
-        scanner.close();
-
-        System.out.println("Coordenadas do ponto 1: (" + pontoUm.getX() + ", " + pontoUm.getY() + ")");
-        System.out.println("Coordenadas do ponto 2: (" + pontoDois.getX() + ", " + pontoDois.getY() + ")");
-
-        double distancia = pontoUm.calcularDistanciaEuclidiana(pontoDois);
-        System.out.println("Distância euclidiana entre os pontos: " + distancia);
-    }
 }
